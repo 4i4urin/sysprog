aa5e827aec755a0b0fa2bf22c67bc1a893ef6bcb
4i4urin <sanekshishurin@gmail.com>
2024-03-10 14:55:32 +0300

    Done task with struct and static array

--- char_dev/char_dev.c
+++ char_dev/char_dev.c
@@ -5,15 +5,10 @@
 #include <linux/uaccess.h>
 
 
-#define MY_MAJOR        31
-#define DEVICE_NAME     "my_chdev2"
+#define MY_MAJOR        30
+#define DEVICE_NAME     "my_chdev"
 
-
-char* phrase = \
-    "The machines rose from the ashes of the nuclear fire.\n" \
-    "Their war to exterminate mankind has raged for decades, but the final battle would not be fought in the future.\n" \
-    "It would be fought here, in our present.\n" \
-    "Tonight.\n";
+#define BUF_LEN         1024
 
 ssize_t my_read(
     struct file* ptr_file, char* user_buff, 
@@ -22,7 +17,7 @@ ssize_t my_read(
 
 ssize_t my_write(
     struct file* ptr_file, const char* user_buff, 
-    size_t count, loff_t* offp
+    size_t length, loff_t* offp
 );
 
 int my_open(struct inode* incode, struct file* ptr_file);
@@ -39,6 +34,12 @@ struct file_operations my_fops = {
 
 struct cdev my_dev;
 
+struct echo {
+    char buf[BUF_LEN];
+    int len;
+} echo_str;
+
+
 
 int init_module(void)
 {
@@ -53,7 +54,7 @@ int init_module(void)
     if (cdev_add(&my_dev, MKDEV(MY_MAJOR, 0), 1) < 0)
         printk(KERN_WARNING "Can't add device\n");
 
-
+    echo_str.len = 0;
     printk(KERN_INFO "Start work\n");
     return 0;
 }
@@ -72,24 +73,21 @@ ssize_t my_read(
     struct file* ptr_file, char* user_buff, 
     size_t length, loff_t* offp) 
 {
-    static uint8_t stop_read = 0;
+    struct echo* echo_str = ptr_file->private_data;
+    int bytes_read = 0;
+
     printk(KERN_INFO "Try read\n");
 
-    if (stop_read == 1)
+    for (size_t i = 0; i < echo_str->len && (echo_str->buf[i] != 0) && i < BUF_LEN; i++)
     {
-        stop_read = 0;
-        return 0;
-    }
-
-    int* some_data = ptr_file->private_data;
-    ssize_t bytes_read = 0;
-    for (size_t i = 0; i < length && (phrase[i] != 0); i++)
-    {
-        put_user(phrase[i], user_buff++);    
+        put_user(echo_str->buf[i], user_buff++);    
         bytes_read++;
     }
-    stop_read += 1;
-    *some_data = bytes_read;
+
+    echo_str->len -= bytes_read;
+    echo_str->buf[echo_str->len] = '\0';
+    
+    printk(KERN_INFO "DEV read %d bytes\n", bytes_read);
     
     return bytes_read;
 }
@@ -97,17 +95,48 @@ ssize_t my_read(
 
 ssize_t my_write(
     struct file* ptr_file, const char* user_buff, 
-    size_t count, loff_t* offp) 
+    size_t length, loff_t* offp) 
 {
+    struct echo* echo_str = ptr_file->private_data;
+    int left = 0;
+
     printk(KERN_INFO "Try write\n");
-    return 0;
+    if (echo_str->len == 0)
+    {
+        if (length >= BUF_LEN)
+            length = BUF_LEN - 1;
+        left = copy_from_user(echo_str->buf, user_buff, length);
+
+        if ( left )
+            printk(KERN_WARNING "WARNING: copy ony part of str\n");
+        echo_str->len = echo_str->len + length - left;
+    }
+    else if (echo_str->len < BUF_LEN - 1)
+    {
+        if (length >= BUF_LEN)
+            length = BUF_LEN - 1;
+        left = copy_from_user(echo_str->buf + echo_str->len - 1, user_buff, length);
+
+        if ( left )
+            printk(KERN_WARNING "WARNING: copy ony part of str\n");
+        echo_str->len = echo_str->len - 1 + length - left;
+    } else
+    {
+        printk(KERN_WARNING "WARNING: Devise buff is full\n");
+        return length;
+    }
+
+    echo_str->buf[echo_str->len] = '\0';
+    printk(KERN_INFO "DEV write %d bytes\n", echo_str->len);
+
+    return length;
 }
 
 
-static int some_data = 0;
 int my_open(struct inode* incode, struct file* ptr_file)
 {
-    ptr_file->private_data = &some_data;
+    ptr_file->private_data = &echo_str;
+
     printk(KERN_INFO "Open device\n");
     return 0;
 }
@@ -115,8 +144,9 @@ int my_open(struct inode* incode, struct file* ptr_file)
 
 int my_close(struct inode* incode, struct file* ptr_file)
 {
-    printk(KERN_INFO "Close device read = %d\n", 
-        *((int*)ptr_file->private_data));
+    struct echo* echo_str = ptr_file->private_data;
+
+    printk(KERN_INFO "Close device STORED = %s\n", echo_str->buf);
     return 0;
 }
 
